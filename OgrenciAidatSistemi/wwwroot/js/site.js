8ebd92c72c2232da27bdea67b00cefaa4c2b8fa3
mal1kc <59576495+mal1kc@users.noreply.github.com>
2024-05-24 18:26:58 +0300

    feat: added color-mode to views

--- OgrenciAidatSistemi/wwwroot/js/site.js
+++ OgrenciAidatSistemi/wwwroot/js/site.js
@@ -2,3 +2,17 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+document.getElementById('Colorscheme_btnSwitch').addEventListener('click', () => {
+    const currentTheme = document.documentElement.getAttribute('data-bs-theme');
+    const switchToTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-bs-theme', switchToTheme);
+    localStorage.setItem('theme', switchToTheme);
+});
+
+// Set the theme on initial load
+
+const storedTheme = localStorage.getItem('theme');
+if (storedTheme) {
+    document.documentElement.setAttribute('data-bs-theme', storedTheme);
+}
